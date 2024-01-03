c02045309de53d5e2d692b7bd394b01721341eba
Don_Reji <donreji88@gmail.com>
2024-01-03 12:52:42 +0530

    changes

--- interview/quckie/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from quckie import views
-
-urlpatterns =[
-    path('tasks/',views.task_list,name='tasks'),
-    path('task/<int:pk>/',views.task_detail,name='task_detail'),
-]
--- interview/test1.py
+++
@@ -1,9 +0,0 @@
-def reverse_string(original):
-    new=original.split()
-    # splits thee strings based on spaces into a list of strings
-
-    return ' '.join(new[::-1])
-    # returns the list of string in reverse order by joining them with spaces
-
-original_string='this is monday'
-print(reverse_string(original_string))
